8dd135db7f13b7890dcd2e5777f1b4c29834cf48
Felix <46428367+efbicief@users.noreply.github.com>
2022-01-26 18:46:16 +0000

    ok

--- s7.sh
+++ s7.sh
@@ -1,8 +1,8 @@
 echo ""
 echo "> nasm -f bin tiny_4.asm"
 nasm -f bin tiny_4.asm
-echo "> ./a.out; echo $?"
-./a.out; echo $?
-echo "> wc -c a.out"
-wc -c a.out
+echo "> ./tiny_4; echo $?"
+./tiny_4; echo $?
+echo "> wc -c tiny_4"
+wc -c tiny_4
 echo ""
--- tiny_2.asm
+++ tiny_2.asm
@@ -2,5 +2,5 @@
 GLOBAL _start ; object symbol used by linker to find entry point
 _start:
         mov     rax, 1 ; exit system call ID, see /usr/include/unistd.h
-        mov     rbx, 2 ; ebx stores argument - value to return to the parent process
-        int     0x80   ; linux system call interface - takes call from eax, parameter from ebx
+        mov     rbx, 2 ; rbx stores argument - value to return to the parent process
+        int     0x80   ; linux system call interface - takes call from rax, parameter from rbx
